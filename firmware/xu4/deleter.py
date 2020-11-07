e86cbac2ce704ca62ee43bbb44e6e66b9a175e72
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2020-11-07 15:21:48 -0600

    Update deleter.py

--- firmware/xu4/deleter.py
+++ firmware/xu4/deleter.py
@@ -6,6 +6,7 @@ from mintsXU4 import mintsSensorReader as mSR
 from mintsXU4 import mintsDefinitions as mD
 
 dataFolder    = mD.dataFolder
+dataFolder    = mD.dataFolderReference
 macAddress    = mD.macAddress
 
 
@@ -25,6 +26,17 @@ def main():
 
         except OSError as e:
             print ("Error: %s - %s." % (e.filename, e.strerror))
+        
+
+    for deleteDate in deleteDays:
+        try:
+            dirPath = os.path.normpath(getDeletePathRef(deleteDate))
+            print("Deleting: "+ dirPath)
+            if os.path.exists(dirPath):
+                shutil.rmtree(dirPath)
+
+        except OSError as e:
+            print ("Error: %s - %s." % (e.filename, e.strerror))
 
 
 def getDeletePath(deleteDate):
@@ -32,6 +44,15 @@ def getDeletePath(deleteDate):
     deletePath = dataFolder+"/"+macAddress+"/"+str(deleteDate.year).zfill(4)  + \
     "/" + str(deleteDate.month).zfill(2)+ "/"+str(deleteDate.day).zfill(2)
     # print(deletePath)
+    return deletePath;
+  
+
+def getDeletePathRef(deleteDate):
+    # deleteDate =  datetime.datetime.now() -  datetime.timedelta(daysBefore)
+    deletePath = dataFolderRef+"/"+macAddress+"/"+str(deleteDate.year).zfill(4)  + \
+    "/" + str(deleteDate.month).zfill(2)+ "/"+str(deleteDate.day).zfill(2)
+    # print(deletePath)
+
     return deletePath;
 
 
