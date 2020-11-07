4c8c57912ccee4d582228c29bd479c63bde71414
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2020-11-07 14:57:51 -0600

    Update deleter.py

--- firmware/xu4/deleter.py
+++ firmware/xu4/deleter.py
@@ -10,18 +10,28 @@ macAddress    = mD.macAddress
 
 
 def main():
+    dateStart      = datetime.date(2016, 12, 6)
     deleteDaysBack = 14
-    try:
-        shutil.rmtree(getDeletePath(deleteDaysBack))
-    except OSError as e:
-        print ("Error: %s - %s." % (e.filename, e.strerror))
+    dateEnd =  datetime.date.today() -  datetime.timedelta(deleteDaysBack)
+
+    deleteDays = [dateStart + datetime.timedelta(days=x) for x in range((dateEnd-dateStart).days + 1)]
+
+    for deleteDate in deleteDays:
+        try:
+            dirPath = os.path.normpath(getDeletePath(deleteDate))
+            print("Deleting: "+ dirPath)
+            if os.path.exists(dirPath):
+                shutil.rmtree(dirPath)
+
+        except OSError as e:
+            print ("Error: %s - %s." % (e.filename, e.strerror))
 
 
-def getDeletePath(daysBefore):
-    deleteDate =  datetime.datetime.now() -  datetime.timedelta(daysBefore)
+def getDeletePath(deleteDate):
+    # deleteDate =  datetime.datetime.now() -  datetime.timedelta(daysBefore)
     deletePath = dataFolder+"/"+macAddress+"/"+str(deleteDate.year).zfill(4)  + \
     "/" + str(deleteDate.month).zfill(2)+ "/"+str(deleteDate.day).zfill(2)
-    print(deletePath)
+    # print(deletePath)
     return deletePath;
 
 
