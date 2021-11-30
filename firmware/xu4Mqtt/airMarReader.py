ff2963088b15f72e7a02edc1ac2a77bc4299f203
lakithaomal <lhw150030@utdallas.edu>
2021-11-30 16:07:52 +0000

    Central Node 2: Updates

--- firmware/xu4Mqtt/airMarReader.py
+++ firmware/xu4Mqtt/airMarReader.py
@@ -1,6 +1,4 @@
 
-
-
 import serial
 import datetime
 from mintsXU4 import mintsSensorReader as mSR
@@ -37,56 +35,55 @@ def main():
     #this will store the line
     line = []
     while True:
+        try:
+            for c in ser.read():
+                line.append(chr(c))
+                if chr(c) == '\n':
+                    dataString     = (''.join(line)).replace("\r\n","")
+                    dateTime  = datetime.datetime.now()
 
-        for c in ser.read():
-            line.append(chr(c))
-            if chr(c) == '\n':
-                dataString     = (''.join(line)).replace("\r\n","")
-                dateTime  = datetime.datetime.now()
-
-                if (dataString.startswith("$HCHDT") and mSR.getDeltaTimeAM(lastHCHDT,delta)):
-                    mSR.HCHDTWriteAM(dataString,dateTime)
-                    lastHCHDT = time.time()
+                    if (dataString.startswith("$HCHDT") and mSR.getDeltaTimeAM(lastHCHDT,delta)):
+                        mSR.HCHDTWriteAM(dataString,dateTime)
+                        lastHCHDT = time.time()
                     # print(str(dataString))
 
-                if (dataString.startswith("$WIMWV") and mSR.getDeltaTimeAM(lastWIMWV,delta)):
-                    mSR.WIMWVWriteAM(dataString,dateTime)
-                    lastWIMWV = time.time()
+                    if (dataString.startswith("$WIMWV") and mSR.getDeltaTimeAM(lastWIMWV,delta)):
+                        mSR.WIMWVWriteAM(dataString,dateTime)
+                        lastWIMWV = time.time()
                     # print(str(dataString))
 
-                if (dataString.startswith("$GPGGA") and mSR.getDeltaTimeAM(lastGPGGA,delta)):
-                    mSR.GPGGAWriteAM(dataString,dateTime)
-                    lastGPGGA = time.time()
+                    if (dataString.startswith("$GPGGA") and mSR.getDeltaTimeAM(lastGPGGA,delta)):
+                        mSR.GPGGAWriteAM(dataString,dateTime)
+                        lastGPGGA = time.time()
                     # print(str(dataString))
 
-                if (dataString.startswith("$GPVTG") and mSR.getDeltaTimeAM(lastGPVTG,delta)):
-                    mSR.GPVTGWriteAM(dataString,dateTime)
-                    lastGPVTG = time.time()
+                    if (dataString.startswith("$GPVTG") and mSR.getDeltaTimeAM(lastGPVTG,delta)):
+                        mSR.GPVTGWriteAM(dataString,dateTime)
+                        lastGPVTG = time.time()
                     # print(str(dataString))
 
-                if (dataString.startswith("$GPZDA") and mSR.getDeltaTimeAM(lastGPZDA,delta)):
-                    mSR.GPZDAWriteAM(dataString,dateTime)
-                    lastGZDA = time.time()
+                    if (dataString.startswith("$GPZDA") and mSR.getDeltaTimeAM(lastGPZDA,delta)):
+                        mSR.GPZDAWriteAM(dataString,dateTime)
+                        lastGZDA = time.time()
                     # print(str(dataString))
 
-                if (dataString.startswith("$WIMDA") and mSR.getDeltaTimeAM(lastWIMDA,delta)):
-                    mSR.WIMDAWriteAM(dataString,dateTime)
-                    lastWIMDA = time.time()
+                    if (dataString.startswith("$WIMDA") and mSR.getDeltaTimeAM(lastWIMDA,delta)):
+                        mSR.WIMDAWriteAM(dataString,dateTime)
+                        lastWIMDA = time.time()
                     # print(str(dataString))
-             #   if (dataString.startswith("$YXXDR,")):
-             #       print("YX1")
-             #       mSR.YXXDRWriteAM(dataString,dateTime)
-             #       lastYXXDR = time.time()
+            
                     
-                    
-                if (dataString.startswith("$YXXDR,") and mSR.getDeltaTimeAM(lastYXXDR,delta)):
-                    mSR.YXXDRWriteAM2(dataString,dateTime)
-                    lastYXXDR = time.time()
+                    if (dataString.startswith("$YXXDR,") and mSR.getDeltaTimeAM(lastYXXDR,delta)):
+                        mSR.YXXDRWriteAM2(dataString,dateTime)
+                        lastYXXDR = time.time()
                     # print(str(dataString))
 
-                line = []
-                break
-
+                    line = []
+                    break
+        except:
+            print("Incomplete String Read")
+            line = []
+                    
     ser.close()
 
 
--- firmware/xu4Mqtt/runAllJetson.sh
+++ firmware/xu4Mqtt/runAllJetson.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 #
 sleep 60
+kill $(pgrep -f 'python3 nanoReader.py 0')
+sleep 10 
 python3 nanoReader.py 0 &
-sleep 5
-python3 airMarReader.py 1 &
-#sleep 5
-#python3 nanoReader.py 2 &
-#sleep 5
-#python3 ips7100ReaderV1.py & 
-#sleep 5 
-#python3 ozoneReader.py &  
-#sleep 5 
-#python3 GPSReader.py &
+sleep 10 
+kill $(pgrep -f 'python3 airMarReader.py')
+sleep 10 
+python3 airMarReader.py &
 sleep 5
 python3 ipReader.py
 
-# python3 centralNodeReaderNano.py &
-# sleep 10
-# cd odroidShow2 && python3 mintsShow2.py
